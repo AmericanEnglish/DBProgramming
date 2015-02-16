f663567c882f65bebac06f970707e4674872430a
Carlos Barajas <12barajasc@gmail.com>
2015-02-16 15:53:30 -0500

    Added some asthetics ZZ

--- cbarajas_hw05.sql
+++ cbarajas_hw05.sql
@@ -5,6 +5,7 @@ FROM
 -- Assuming available = 1 means the copy is ready to be checked out
 -- and available = 0 means the copy cannot be checked out
 WHERE available = 1 AND title = 'Lord of the Rings';
+
 --6.46
 SELECT borrowername
 FROM book
@@ -12,28 +13,33 @@ FROM book
     INNER JOIN bookloan ON bookloan.copyno = bookcopy.copyno
     INNER JOIN borrower ON borrower.borrowerno = bookloan.borrowerno
 WHERE title = 'Lord of the Rings';
+
 --6.47
 SELECT DISTINCT borrowername
     FROM bookloan
         INNER JOIN borrower ON bookloan.borrowerno = borrower.borrowerno
 WHERE date(datedue) < date('now');
+
 --6.48
 SELECT count(ISBN)
     FROM bookcopy
     GROUP BY  ISBN
     HAVING ISBN = '0-321-52306-7';
+
 --6.49
 SELECT count(bookcopy.ISBN) - count(bookloan.dateout) AS 'remaining'
 FROM bookcopy
     LEFT JOIN  bookloan ON bookcopy.copyno = bookloan.copyno
     GROUP BY bookcopy.ISBN
     HAVING ISBN = '0-321-52306-7';
+
 --6.50
 SELECT COUNT(ISBN)
 FROM  bookcopy
     INNER JOIN bookloan ON bookcopy.copyno = bookloan.copyno
     GROUP BY ISBN
     HAVING ISBN = '0-321-52306-7';
+
 --6.51
 SELECT title, bookcopy.ISBN, bookcopy.copyno
 FROM book
@@ -41,6 +47,7 @@ FROM book
     INNER JOIN bookloan ON bookloan.copyno = bookcopy.copyno
     INNER JOIN borrower ON borrower.borrowerno = bookloan.borrowerno
 WHERE borrowername = 'Peter Bloomfield';
+
 --6.52
 SELECT borrowername, title
 FROM bookcopy
@@ -48,11 +55,13 @@ FROM bookcopy
     INNER JOIN bookloan ON bookcopy.copyno = bookloan.copyno
     INNER JOIN borrower ON bookloan.borrowerno = borrower.borrowerno
 WHERE ______
+
 --6.53
 SELECT borrowerno, borrowername, borroweraddress
 FROM  bookloan
     INNER JOIN borrower ON bookloan.borrowerno = borrower.borrowerno
 WHERE date(datedue) < date('now')
+
 --6.54
 SELECT title, COUNT(bookloan.copyno) AS 'number borrowed'
 FROM book
