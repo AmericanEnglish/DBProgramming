1bd627c95757f54dba648129c0f8394216688f45
Carlos Barajas <12barajasc@gmail.com>
2015-02-23 02:45:26 -0500

    Added Hotel guest

--- samplepostgress.sql
+++ samplepostgress.sql
@@ -5,3 +5,11 @@ INSERT INTO "Hotel Database"."Booking" VALUES
 (2, 4, 'Jan-08-2014', 'Jan-20-2014', 1),
 (2, 5, 'Jan-08-2014', 'Jan-20-2014', 2),
 (2, 6, 'Jan-08-2014', 'Jan-20-2014', 3);
+--Random Guests
+INSERT INTO "Hotel Database"."Guest" VALUES
+(1, 'Guest A', 'Address 1'),
+(2, 'Guest B', 'Address 2'),
+(3, 'Guest C', 'Address 3'),
+(4, 'Guest D', 'Address 4'),
+(5, 'Guest E', 'Address 5'),
+(6, 'Guest F', 'Address 6');
