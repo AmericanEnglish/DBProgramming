323fc0f581802d76aec8365adb4baaf992c40557
Carlos Barajas <12barajasc@gmail.com>
2015-02-23 14:10:22 -0500

    Cleaning up syntax to fit ISO

--- cbarajas_hw06.sql
+++ cbarajas_hw06.sql
@@ -1,53 +1,39 @@
 -- 7.10
-CREATE TABLE "Hotel Database"."Hotel"
+CREATE TABLE "hotel"
 (
-    "hotelNo" integer NOT NULL,
-    "hotelName" character varying(10), -- Name for the name of the hotels
-    city character varying(10), -- Tells which city the hotel is in
-    CONSTRAINT "hotelNo" PRIMARY KEY ("hotelNo")
+    "hotelNo" INTEGER NOT NULL PRIMARY KEY,
+    "hotelName" VARCHAR(10), -- Name for the name of the hotels
+    "city" VARCHAR(10) -- Tells which city the hotel is in
 )
-WITH (
-    OIDS=FALSE
-);
-ALTER TABLE "Hotel Database"."Hotel"
-    OWNER TO postgres;
 
 -- 7.11 (2 points)
 
-CREATE TABLE "Hotel Database"."Room"
+CREATE TABLE "room"
 (
-    "roomNo" integer NOT NULL,
-    "hotelNo" integer NOT NULL,
-    "type" character(6),
-    "price" money,
+    "roomNo" INTEGER NOT NULL PRIMARY KEY,
+    "hotelNo" INTEGER NOT NULL PRIMARY KEY,
+    "type" CHAR(6),
+    "price" MONEY,
     CONSTRAINT "roomNo limits" CHECK ("roomNo" > 0 AND "roomNo" < 101),
     CONSTRAINT "type can be 1 of 3" CHECK ("type" = 'Single' OR "type" = 'Double' OR "type" = 'Family'),
-    CONSTRAINT "Price range" CHECK (price > money(10) AND price < money(100)),
-    CONSTRAINT "Room P Key" PRIMARY KEY ("roomNo", "hotelNo"),
+    CONSTRAINT "Price range" CHECK (price > MONEY(10) AND price < MONEY(100)),
     CONSTRAINT "Room F Key" FOREIGN KEY ("hotelNo")
-        REFERENCES "Hotel Database"."Hotel" ("hotelNo")
+        REFERENCES "Hotel" ("hotelNo")
         ON UPDATE NO ACTION ON DELETE NO ACTION
-)
-WITH (
-  OIDS=FALSE
-);
-ALTER TABLE "Hotel Database"."Room"
-    OWNER TO postgres;
 
-
-CREATE TABLE "Hotel Database"."Booking"
+CREATE TABLE "Booking"
 (
-    "hotelNo" integer NOT NULL,
-    "guestNo" integer NOT NULL,
+    "hotelNo" INTEGER NOT NULL,
+    "guestNo" INTEGER NOT NULL,
     "dateFrom" date NOT NULL,
     "dateTo" date,
-    "roomNo" integer,
+    "roomNo" INTEGER,
     CONSTRAINT "Booking P Key" PRIMARY KEY ("hotelNo", "guestNo", "dateTo"),
     CONSTRAINT "Booking F Key" FOREIGN KEY ("hotelNo", "roomNo")
-        REFERENCES "Hotel Database"."Room" ("hotelNo", "roomNo")
+        REFERENCES "Room" ("hotelNo", "roomNo")
         ON UPDATE NO ACTION ON DELETE NO ACTION,
     CONSTRAINT "Booking F Key guestNo" FOREIGN KEY ("guestNo")
-        REFERENCES  "Hotel Database"."Guest" ("guestNo")
+        REFERENCES  "Guest" ("guestNo")
         ON UPDATE NO ACTION ON DELETE NO ACTION,
     CONSTRAINT "Date validity" CHECK ("dateTo" > date('today') AND "dateFrom" > date('today')),
     -- CONSTRAINT "No double Booking" CHECK ()
@@ -55,35 +41,35 @@ CREATE TABLE "Hotel Database"."Booking"
 WITH (
   OIDS=FALSE
 );
-ALTER TABLE "Hotel Database"."Booking"
+ALTER TABLE "Booking"
     OWNER TO postgres;
 -- 7.12 (include your INSERT/DELETE statements)
 
-CREATE TABLE "Hotel Database"."Archive"
+CREATE TABLE "Archive"
 (
-    "hotelNo" integer NOT NULL
-    "guestNo" integer NOT NULL
+    "hotelNo" INTEGER NOT NULL
+    "guestNo" INTEGER NOT NULL
     "dateFrom" date NOT NULL
     "dateTo" date 
-    "roomNo" integer 
+    "roomNo" INTEGER 
     CONSTRAINT "Archive P Keys" PRIMARY KEY ("hotelNo", "guestNo", "dateFrom")
     CONSTRAINT "Archive F Keys" FOREIGN KEY ("roomNo", "hotelNo")
-        REFERENCES "Hotel Database"."Room" ("roomNo", "hotelNo")
+        REFERENCES "Room" ("roomNo", "hotelNo")
         ON UPDATE NO ACTION ON DELETE NO ACTION
     CONSTRAINT "Archive Guest Number" FOREIGN KEY ("guestNo")
-        REFERENCES "Hotel Database"."Guest" ("guestNo")
+        REFERENCES "Guest" ("guestNo")
         ON UPDATE NO ACTION ON DELETE NO ACTION
 
 );
   OIDS=FALSE
 );
-ALTER TABLE "Hotel Database"."Archive"
+ALTER TABLE "Archive"
     OWNER TO postgres;
 -- 7.13
 CREATE VIEW "PresentGuests" 
-    AS SELECT "hotelName", "guestName" FROM "Hote Database"."Booking"
-        INNER JOIN "Hotel Database"."Guest" ON "Booking"."guestNo" = "Guest"."guestNo"
-        INNER JOIN "Hotel Database"."Hotel" ON "Booking"."hotelNo" = "Hotel"."hotelNo";
+    AS SELECT "hotelName", "guestName" FROM"Booking"
+        INNER JOIN "Guest" ON "Booking"."guestNo" = "Guest"."guestNo"
+        INNER JOIN "Hotel" ON "Booking"."hotelNo" = "Hotel"."hotelNo";
 
 -- 7.14
 
--- samplepostgress.sql
+++ samplepostgress.sql
@@ -1,4 +1,4 @@
-INSERT INTO "Hotel Database"."Booking" VALUES
+INSERT INTO "Booking" VALUES
 (1, 1, 'Jan-08-2014', 'Jan-20-2014', 1),
 (1, 2, 'Jan-08-2014', 'Jan-20-2014', 2),
 (1, 3, 'Jan-08-2014', 'Jan-20-2014', 3),
@@ -6,7 +6,7 @@ INSERT INTO "Hotel Database"."Booking" VALUES
 (2, 5, 'Jan-08-2014', 'Jan-20-2014', 2),
 (2, 6, 'Jan-08-2014', 'Jan-20-2014', 3);
 --Random Guests
-INSERT INTO "Hotel Database"."Guest" VALUES
+INSERT INTO "Guest" VALUES
 (1, 'Guest A', 'Address 1'),
 (2, 'Guest B', 'Address 2'),
 (3, 'Guest C', 'Address 3'),
@@ -14,7 +14,7 @@ INSERT INTO "Hotel Database"."Guest" VALUES
 (5, 'Guest E', 'Address 5'),
 (6, 'Guest F', 'Address 6');
 
-INSERT INTO "Hotel Database"."Room" VALUES
+INSERT INTO "Room" VALUES
 (1, 1, 'Single', 20),
 (2, 1, 'Double', 30),
 (3, 1, 'Family', 50),
