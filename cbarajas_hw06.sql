136d01e0c9c0d63455c116d088d48b92a6d53933
Carlos Barajas <12barajasc@gmail.com>
2015-02-23 23:29:15 -0500

    Finished

--- cbarajas_hw06.sql
+++ cbarajas_hw06.sql
@@ -87,7 +87,7 @@ CREATE VIEW presentguests
 
 -- 7.14
 CREATE VIEW grosvenorguests
-    AS SELECT hotelName, allbookings.roomNo, guest.guestNo, allbookings.dateFrom, allbookings.dateTo FROM  
+    AS SELECT hotelName, allbookings.roomNo, guest.guestNo, guest.guestName, allbookings.dateFrom, allbookings.dateTo FROM  
     (SELECT * FROM booking UNION SELECT * FROM archive) AS allbookings 
         INNER JOIN guest ON allbookings.guestNo = guest.guestNo
         INNER JOIN hotel ON allbookings.hotelno = hotel.hotelno
@@ -106,5 +106,9 @@ ON presentguests, grosvenorguests
 FROM Accounts;
 -- 7.17 (2 points)
     --a.) Valid, pulls the hotelno from hotel and then the count value from the select statement (which targets the booking table)
-    --b.)
+    --b.) Valid, pulls the hotelno. The query maps over both hotel and room because the hotelnos must exist in both
+    --c.) Valid, It selects the minimum value from the view. This maps over the booking and room on roomno and then see which hotel has the minimum guests book by rooms booked
+    --d.) Valid, maps over hotel, room, and booking counting only where the roomnos of room and booking match up as well as the hotelno and the roomno match up
+    --e.) Valid,  Combs over hotel and room where the hotelnos match up and then combs over room and booking where the sum of booking.roomno and room.roomno  for one hotel is greater than 1k
+    --f.) Valid, It seems that selecting hotelno from hotelbookingcount maps over all three but the select maps over hotel and room because it is pulling values where h.hotelno = r.hotelno. The orderby maps over booking and room where their numbers are equivalent
 -- Turn in a SQL file with DDL/DML for the assignment. Test your queries using PostgreSQL.
