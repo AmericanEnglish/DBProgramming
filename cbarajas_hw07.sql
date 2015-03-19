da4b21e60b7e6d0ac29c3f0d221d0ec72e48e61e
Carlos Barajas <12barajasc@gmail.com>
2015-03-19 18:05:54 -0400

    last lab commit

--- cbarajas_hw07.sql
+++ cbarajas_hw07.sql
@@ -1,5 +1,4 @@
 
--- (2 points) Write a stored procedure for exercise 6.8, making city a parameter.
-
--- (2 points) Write a stored procedure for exercise 6.10, making the price a parameter.
 -- (2 points each) Complete exercises 8.11 a, c, e.
+-- (2 points) Write a stored procedure for exercise 6.10, making the price a parameter.
+-- (2 points) Write a stored procedure for exercise 6.8, making city a parameter.
