4a9b6dae7a06edd5c6b5677dae523727decd1173
Carlos Barajas <12barajasc@gmail.com>
2015-03-19 20:34:42 -0400

    Finished problem 2

--- cbarajas_hw07.sql
+++ cbarajas_hw07.sql
@@ -1,6 +1,6 @@
 -- (2 points) Write a stored procedure for exercise 6.8, making city a parameter.
 --   List full details of all hotels in london
-CREATE PROCEDURE londonhotels 
+CREATE PROCEDURE london_hotels
 AS 
 SELECT hotelNo, hotelName
 FROM hotel 
@@ -9,6 +9,14 @@ WHERE city = 'London';
 -- (2 points) Write a stored procedure for exercise 6.10, making the price a parameter.
 --   List all double or family rooms with a price below 40.00 euro per night in ascending order of price
 
+CREATE PROCEDURE singles_doubles_lt40
+AS
+SELECT room.hotelName, roomNo, price
+FROM room
+    INNER JOIN hotel ON room.hotelNo = hotel.hotelNo
+WHERE price < money(40) 
+    AND (type = 'Double' OR type = 'Family')
+ORDER BY price
 
 -- (2 points each) Complete exercises 8.11 a, c, e. create triggers for
 -- a.  price of all double rooms greater than 100 euro
