69ef7a71a7eb59714161a1f62c0fb15ad72011c9
Carlos Barajas <12barajasc@gmail.com>
2015-03-19 20:11:38 -0400

    Changed order of problems

--- cbarajas_hw07.sql
+++ cbarajas_hw07.sql
@@ -1,4 +1,10 @@
-
--- (2 points each) Complete exercises 8.11 a, c, e.
--- (2 points) Write a stored procedure for exercise 6.10, making the price a parameter.
 -- (2 points) Write a stored procedure for exercise 6.8, making city a parameter.
+--   List full details of all hotels in london
+
+-- (2 points) Write a stored procedure for exercise 6.10, making the price a parameter.
+--   List all double or family rooms with a price below 40.00 euro per night in ascending order of price
+
+
+-- (2 points each) Complete exercises 8.11 a, c, e. create triggers for
+-- a.  price of all double rooms greater than 100 euro
+-- c. 
