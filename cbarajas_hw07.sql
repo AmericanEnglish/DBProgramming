c9d2c12790a551a7b1e7cc61cafd64949c853a51
Carlos Barajas <12barajasc@gmail.com>
2015-03-19 20:26:20 -0400

    Finished first problem

--- cbarajas_hw07.sql
+++ cbarajas_hw07.sql
@@ -1,5 +1,10 @@
 -- (2 points) Write a stored procedure for exercise 6.8, making city a parameter.
 --   List full details of all hotels in london
+CREATE PROCEDURE londonhotels 
+AS 
+SELECT hotelNo, hotelName
+FROM hotel 
+WHERE city = 'London';
 
 -- (2 points) Write a stored procedure for exercise 6.10, making the price a parameter.
 --   List all double or family rooms with a price below 40.00 euro per night in ascending order of price
